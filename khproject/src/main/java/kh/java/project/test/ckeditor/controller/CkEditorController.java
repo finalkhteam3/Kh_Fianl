2c8600c6ba39377566f4f5c87d669da825ab0fd7
bekky1016 <110219144+bekky1016@users.noreply.github.com>
2023-01-28 13:48:13 +0900

    smarteditor 삭제

--- khproject/src/main/java/kh/java/project/test/ckeditor/controller/CkEditorController.java
+++ khproject/src/main/java/kh/java/project/test/ckeditor/controller/CkEditorController.java
@@ -1,14 +1,14 @@
-package kh.java.project.test.smarteditor2;
+package kh.java.project.test.ckeditor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SmartEditor2Controller {
-
-    @GetMapping("/se2")
+public class CkEditorController {
+	
+	@GetMapping("/ck")
     public String smarteditor2(){
-        return "smarteditor2/se2";
+        return "ckeditor/ck";
     }
-
+	
 }
