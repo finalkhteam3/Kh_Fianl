da61b85d57414ab5a3b776e3a29ee07272c7d1ac
bekky1016 <110219144+bekky1016@users.noreply.github.com>
2023-01-30 00:12:40 +0900

    ckeditor 파일업로드 (진행중)
    파일 서버에 업로드까지만 가능..
    글 등록은 아직..

--- khproject/src/main/java/kh/java/project/test/ckeditor/controller/CkEditorController.java
+++ khproject/src/main/java/kh/java/project/test/ckeditor/controller/CkEditorController.java
@@ -1,14 +1,148 @@
 package kh.java.project.test.ckeditor.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import kh.java.project.member.dto.request.MemberRequest;
+
 
 @Controller
 public class CkEditorController {
-	
+
 	@GetMapping("/ck")
-    public String smarteditor2(){
-        return "ckeditor/ck";
+	public String smarteditor2() {
+		return "ckeditor/ck";
+	}
+
+//	@PostMapping("/ck")
+//    public String join(MemberRequest request) {
+//        memberService.createMember(request);
+//        return "redirect:/ckboard";
+//    }
+	
+	// 이미지 업로드
+    @RequestMapping(value="imageUpload.do", method = RequestMethod.POST)
+    public void imageUpload(HttpServletRequest request,
+    		HttpServletResponse response, MultipartHttpServletRequest multiFile
+    		, @RequestParam MultipartFile upload) throws Exception{
+    	// 랜덤 문자 생성
+    	UUID uid = UUID.randomUUID();
+    	
+    	OutputStream out = null;
+    	PrintWriter printWriter = null;
+    	
+    	//인코딩
+    	response.setCharacterEncoding("utf-8");
+    	response.setContentType("text/html;charset=utf-8");
+    	try{
+    		//파일 이름 가져오기
+    		String fileName = upload.getOriginalFilename();
+    		byte[] bytes = upload.getBytes();
+    		
+    		//이미지 경로 생성
+    		String path = "C:\\z_workspace" + "ckImage/";	// 이미지 경로 설정(폴더 자동 생성)
+    		String ckUploadPath = path + uid + "_" + fileName;
+    		File folder = new File(path);
+    		System.out.println("path:"+path);	// 이미지 저장경로 console에 확인
+    		//해당 디렉토리 확인
+    		if(!folder.exists()){
+    			try{
+    				folder.mkdirs(); // 폴더 생성
+    		}catch(Exception e){
+    			e.getStackTrace();
+    		}
+    	}
+    	
+    	out = new FileOutputStream(new File(ckUploadPath));
+    	out.write(bytes);
+    	out.flush(); // outputStram에 저장된 데이터를 전송하고 초기화
+    	
+    	String callback = request.getParameter("CKEditorFuncNum");
+    	printWriter = response.getWriter();
+    	String fileUrl = "/ckImgSubmit.do?uid=" + uid + "&fileName=" + fileName; // 작성화면
+    	
+    	// 업로드시 메시지 출력
+    	printWriter.println("{\"filename\" : \""+fileName+"\", \"uploaded\" : 1, \"url\":\""+fileUrl+"\"}");
+    	printWriter.flush();
+    	
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	} finally {
+    		try {
+    		if(out != null) { out.close(); }
+    		if(printWriter != null) { printWriter.close(); }
+    	} catch(IOException e) { e.printStackTrace(); }
+    	}
+    	return;
     }
 	
+ // 서버로 전송된 이미지 뿌려주기
+    @RequestMapping(value="/ckImgSubmit.do")
+    public void ckSubmit(@RequestParam(value="uid") String uid
+    		, @RequestParam(value="fileName") String fileName
+    		, HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+    	
+    	//서버에 저장된 이미지 경로
+    	String path = "C:\\z_workspace" + "ckImage/";	// 저장된 이미지 경로
+    	System.out.println("path:"+path);
+    	String sDirPath = path + uid + "_" + fileName;
+    	
+    	File imgFile = new File(sDirPath);
+    	
+    	//사진 이미지 찾지 못하는 경우 예외처리로 빈 이미지 파일을 설정한다.
+    	if(imgFile.isFile()){
+    		byte[] buf = new byte[1024];
+    		int readByte = 0;
+    		int length = 0;
+    		byte[] imgBuf = null;
+    		
+    		FileInputStream fileInputStream = null;
+    		ByteArrayOutputStream outputStream = null;
+    		ServletOutputStream out = null;
+    		
+    		try{
+    			fileInputStream = new FileInputStream(imgFile);
+    			outputStream = new ByteArrayOutputStream();
+    			out = response.getOutputStream();
+    			
+    			while((readByte = fileInputStream.read(buf)) != -1){
+    				outputStream.write(buf, 0, readByte); 
+    			}
+    			
+    			imgBuf = outputStream.toByteArray();
+    			length = imgBuf.length;
+    			out.write(imgBuf, 0, length);
+    			out.flush();
+    			
+    		}catch(IOException e){
+    			e.printStackTrace();
+    		}finally {
+    			outputStream.close();
+    			fileInputStream.close();
+    			out.close();
+    			}
+    		}
+    }
+    
 }
--- khproject/src/main/webapp/WEB-INF/views/ckeditor/ck.jsp
+++ khproject/src/main/webapp/WEB-INF/views/ckeditor/ck.jsp
@@ -5,25 +5,37 @@
 <head>
 <meta charset="UTF-8">
 <title>ckeditor test</title>
-<script src="<%=request.getContextPath()%>/resources/ckeditor/ckeditor.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script src="<%=request.getContextPath()%>/resources/ckeditor/ckeditor.js"></script>
 </head>
 <body>
 <h3>ckeditor test</h3>
 
-	<textarea name="ckeditor" id="ckeditor"></textarea>
+<form action="<%=request.getContextPath()%>/ckboard" method="post"	enctype="multipart/form-data" id="frm">
+		Title:<input type="text" name="boardTitle" id="boardTitle"> 
+		<br> 
+		Writer:<input type="text" name="boardWriter" id="boardWriter"> 
+		<br> 
+		Content:
+		<textarea name="ckeditor" id="ckeditor"></textarea>
+		<button id="savebutton">글등록</button>
+	</form>
 
 
-<script type="text/javascript">
-	var ckeditor_config = {
-	      resize_enaleb : false,
-	      enterMode : CKEDITOR.ENTER_BR,
-	      shiftEnterMode : CKEDITOR.ENTER_P
-	      /* , */
-	      /* filebrowserUploadUrl : "이미지 업로드 처리를 하는 controller명" */
-	      };
+
+
+<script type="text/javascript">	// 글쓰기 editor 및 사진 업로드 기능
+			CKEDITOR.replace('ckeditor',
+			{filebrowserUploadUrl:'imageUpload.do'
+			});
+		</script>
+
+<%-- 	$(function(){
+	CKEDITOR.replace('ckeditor',{
+		filebrowserUploadUrl : '<%=request.getContextPath()%>/ckeditor/fileupload.do'
+	});
+});  --%>
 	
-	CKEDITOR.replace("ckeditor",ckeditor_config);
 </script>
 </body>
 </html>
