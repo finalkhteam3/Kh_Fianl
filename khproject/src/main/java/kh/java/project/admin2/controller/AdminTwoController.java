2ea5d76215b2430f75f6fb912274bd9ac6587af0
anmingyu0101 <zndn5263@gmail.com>
2023-02-02 19:02:14 +0900

    admin2
    /admin2

---
+++ khproject/src/main/java/kh/java/project/admin2/controller/AdminTwoController.java
@@ -0,0 +1,33 @@
+package kh.java.project.admin2.controller;
+
+import org.springframework.stereotype.Controller;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kh.java.project.HomeController;
+
+@Controller
+public class AdminTwoController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/admin2", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+		return "admin2";
+	}
+}
