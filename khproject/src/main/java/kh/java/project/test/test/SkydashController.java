f4ee721c3170fc4aaae9b6bca29bd4ff19fbd322
bekky1016 <110219144+bekky1016@users.noreply.github.com>
2023-02-09 03:21:45 +0900

    js 경로 변경/ test url 변경/ 메인 화면 ajax DB 데이터 출력 진행중
    작업중인 메인 url: /jira/your-work

--- khproject/src/main/java/kh/java/project/test/test/SkydashController.java
+++ khproject/src/main/java/kh/java/project/test/test/SkydashController.java
@@ -3,20 +3,18 @@ package kh.java.project.test.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kh.java.project.HomeController;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class SkydashController {
-private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/index_skydash", method = RequestMethod.GET)
-	public String adminOne() {
-		return "/skydash_temp/index_skydash";
+	@GetMapping("/jira/your-work")
+	public String getIndexPage() {
+		return "skydash_temp/index_skydash";
 	}
 }
--- khproject/src/main/webapp/WEB-INF/views/component/header2.jsp
+++ khproject/src/main/webapp/WEB-INF/views/component/header2.jsp
@@ -9,9 +9,9 @@
 <body>
 	<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="/resources/skydashTemp/images/logo.png"
+            <a class="navbar-brand brand-logo mr-5" href="your-work"><img src="/resources/skydashTemp/images/logo.png"
                                                                            class="mr-2" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="index.html"><img
+            <a class="navbar-brand brand-logo-mini" href="your-work"><img
                     src="/resources/skydashTemp/images/logo-mini.svg" alt="logo"/></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
